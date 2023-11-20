2b9992f9966c1bee0466866f290ba6a3c004aa2c
VIKASH SAINI <143334443+vikash-saini-maruti@users.noreply.github.com>
2023-11-20 17:18:55 +0530

    Update admin.py

--- Lakhme Royal Residency Project/service/admin.py
+++ Lakhme Royal Residency Project/service/admin.py
@@ -5,6 +5,6 @@ from django.contrib import admin
 from service.models import Service
 
 class ServiceAdmin(admin.ModelAdmin):
-    list_display=('servoce_icon','service_title')
+    list_display=('service_icon','service_title')
 
 admin.site.register(Service,ServiceAdmin)
